161ebbf06127249ea85f5512381a542627cee222
Dastan <kasymbekovdastan15@gmail.com>
2022-12-08 12:21:14 +0600

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,20 @@ import dotenv = require('dotenv');
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+  // await app.listen(process.env.PORT || 4000);
+  const config = new DocumentBuilder()
+    .setTitle(' example')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('solvice')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
